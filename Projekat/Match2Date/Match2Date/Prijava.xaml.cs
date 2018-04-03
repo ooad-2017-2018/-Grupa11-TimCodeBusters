c9eaa31b6ad0acd95f913ff4a6ababc1e0999804
Askelov <email@email.com>
2018-04-04 01:36:27 +0200

    zavrsena login forma

--- Projekat/Match2Date/Match2Date/Prijava.xaml.cs
+++ Projekat/Match2Date/Match2Date/Prijava.xaml.cs
@@ -26,5 +26,10 @@ namespace Match2Date
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
