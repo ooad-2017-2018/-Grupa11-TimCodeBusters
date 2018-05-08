f7bb4379ebd52c0dacbbe2f07a998f7e7d875708
Hamza Begovic <hbegovic1@etf.unsa.ba>
2018-05-08 14:09:31 +0200

    popravke

--- Projekat/Match2Date/Match2Date/App.xaml.cs
+++ Projekat/Match2Date/Match2Date/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//using Microsoft.Data.Entity;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -24,8 +25,7 @@ namespace Match2Date
     /// </summary>
     sealed partial class App : Application
     {
-        public static MobileServiceClient MobileService = new
-        MobileServiceClient("https://Match2Date.azurewebsites.net");
+        public static MobileServiceClient MobileService = new MobileServiceClient("https://Match2Date.azurewebsites.net");
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
