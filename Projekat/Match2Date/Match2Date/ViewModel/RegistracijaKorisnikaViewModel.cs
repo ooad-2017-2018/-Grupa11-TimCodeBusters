056f6c40ff930672c87b989c722e3b1bc3c2dc21
Hamza Begovic <hbegovic1@etf.unsa.ba>
2018-05-09 21:45:34 +0200

    registracija korisnika view model

--- Projekat/Match2Date/Match2Date/ViewModel/RegistracijaKorisnikaViewModel.cs
+++ Projekat/Match2Date/Match2Date/ViewModel/RegistracijaKorisnikaViewModel.cs
@@ -3,22 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 using Match2Date.Model;
 using Match2Date.Enumeration;
 
 namespace Match2Date.ViewModel
 {
-    public class RegistracijaKorisnikaViewModel
+    public class RegistracijaKorisnikaViewModel : INotifyPropertyChanged
     {
-        public string VIme { get; set; }
-        public string VPrezime { get; set; }
-        public string VGrad { get; set; }
-        public string VEmail { get; set; }
-        public string VSifra { get; set; }
-        public string VPotSifra { get; set; }
-        public DateTime VDatumRodjenja { get; set; }
-        public Spol VSpol { get; set; }
-        public string VOpis { get; set; }
-        List<Byte[]> VListaSlika { get; set; }
+        private string _vIme;
+        private string _vPrezime;
+        private string _vGrad;
+        private string _vEmail;
+        private string _vSifra;
+        private string _vPotSifra;
+        private DateTime _vDatumRodjenja;
+        private Spol _vSpol;
+        private string _vOpis;
+
+        public string VIme { get => _vIme; set { _vIme = value; NotifyPropertyChanged(nameof(VIme)); } }
+        public string VPrezime { get => _vPrezime; set { _vPrezime = value; NotifyPropertyChanged(nameof(VPrezime)); } }
+        public string VGrad { get => _vGrad; set { _vGrad = value; NotifyPropertyChanged(nameof(VGrad)); } }
+        public string VEmail { get => _vEmail; set { _vEmail = value; NotifyPropertyChanged(nameof(VEmail)); } }
+        public string VSifra { get => _vSifra; set { _vSifra = value; NotifyPropertyChanged(nameof(VSifra)); } }
+        public string VPotSifra { get => _vPotSifra; set { _vPotSifra = value; NotifyPropertyChanged(nameof(VPotSifra)); } }
+        public DateTime VDatumRodjenja { get => _vDatumRodjenja; set { _vDatumRodjenja = value; NotifyPropertyChanged(nameof(VDatumRodjenja)); } }
+        public Spol VSpol { get => _vSpol; set { _vSpol = value; NotifyPropertyChanged(nameof(VSpol)); } }
+        public string VOpis { get => _vOpis; set { _vOpis = value; NotifyPropertyChanged(nameof(VOpis)); } }
+        //List<Byte[]> VListaSlika { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
     }
 }
