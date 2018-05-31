696eda5b0b9b336f0d75a39065b322369743945a
Hamza Begovic <hbegovic1@etf.unsa.ba>
2018-05-31 13:41:35 +0200

    kreiran asp

---
+++ Projekat/Match2DateAsp/Match2DateAsp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Match2DateAsp.Startup))]
+namespace Match2DateAsp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
