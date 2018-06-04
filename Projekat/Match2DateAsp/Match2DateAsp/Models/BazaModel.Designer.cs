309e0bca5ba5827cecf6a783bb47fb780b3791de
Askelov <email@email.com>
2018-06-04 14:49:50 +0200

    a

--- Projekat/Match2DateAsp/Match2DateAsp/Models/BazaModel.Designer.cs
+++ Projekat/Match2DateAsp/Match2DateAsp/Models/BazaModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Hp\Desktop\paradigm\Match2DateAsp\Match2DateAsp\Models\BazaModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\User\Desktop\Grupa11-TimCodeBusters\Projekat\Match2DateAsp\Match2DateAsp\Models\BazaModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
